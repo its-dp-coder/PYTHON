5d7b9aca9152e9655549038bf4c6b7f394a13e40
Dependra Pratap Singh <dependrapratapsingh56@gmail.com>
2026-07-02 10:55:26 +0530

    upload python folder starting a python learning journey

---
+++ 01-basics/hello_world.py
@@ -0,0 +1,7 @@
+print("hello ")
+def greet(name):
+    print(name)
+greet("Alice")
+def caste_name(caste):
+    print(caste)
+caste_name("Rajput")    
---
+++ 01-basics/raj.py
@@ -0,0 +1,3 @@
+from hello_world import greet, caste_name
+greet("Bob")
+caste_name("Brahmin")
